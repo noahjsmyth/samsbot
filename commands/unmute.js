aaa3b8efed9259d4059e055699009273cd5699b6
Noah Smyth <noahjohnsmyth@gmail.com>
2021-03-11 16:39:59 +0000

    Update unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -3,25 +3,33 @@ module.exports = {
 	name: 'unmute',
 	description: 'Removes a users mute role.',
 	execute(message, args) {
-		let reason = args.slice(1).join(' ');
-		let user = message.mentions.users.first();
-		let role = message.guild.roles.find('name', 'Muted')
-		
+	    let noPermission = new Discord.RichEmbed()
+        .setAuthor(`No Permission`, message.guild.iconURL)
+        .setColor(0x421C52)
+        .setDescription(`${message.author}, I don't have permission to add roles, contact the staff!`)
+        .setFooter(`Check in the mute.js file if there is an error!`)
+    if(!message.guild.me.hasPermission(["MANAGE_ROLES", "ADMINISTRATOR"])) return message.channel.send(noPermission)
     
-   		if (!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send(":no_entry_sign: **Error:** You don't have the **Manage Roles** permission!");
-    		if (!message.guild.member(user).roles.has(role)) return message.channel.send(`:no_entry_sign: I cannot unmute that member`);
-    		message.guild.member(user).removeRole(role);
-
-    		const embed = new Discord.RichEmbed()
-      			.setColor("0xFF0000")
-      			.setTimestamp()
-      			.addField('Action:', 'Unmute')
-      			.addField('User:', `${user.tag} (${user.id})`)
-      			.addField('Moderator:', 
-      `			${message.author.tag}`)
-      			.addField('Reason', reason);
-      		return message.channel.send(embed);
-
-
+    let mutee = message.mentions.members.first() || message.guild.members.get(args[0])
+        if(!mutee) return message.channel.send('You have to @ a user in this discord!')
+        let reason = args.slice(1).join(" ");
+        if(!reason) reason = "No reason given"
+    let mutedRole = message.guild.roles.find(r => r.name === "Muted")
+    if(!mutedRole) return message.channel.send('The user you @ is not muted!')
+    mutee.removeRole(mutedRole.id).then(() => {
+        message.delete()
+        let unmutePing = new Discord.RichEmbed()
+            .setAuthor(`UnMuted`, message.guild.iconURL)
+            .setColor(0x421C52)
+            .setDescription(`${message.author}, you have been unmuted for: ${reason}!`)
+            .setFooter(`Check in the unmute.js file if there is an error!`)
+        mutee.send(unmutePing).catch(err => console.log(err))
+        let channelunmutePing = new Discord.RichEmbed()
+        .setAuthor(`UnMuted`, message.guild.iconURL)
+        .setColor(0x421C52)
+        .setDescription(`${message.author}, has been unmuted for: ${reason}!`)
+        .setFooter(`Check in the unmute.js file if there is an error!`)
+        message.channel.send(channelunmutePing)
+    })
 	},
 };
