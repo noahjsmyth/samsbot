9e372af0a68fe3062de011b2d4af309533b8631c
Noah Smyth <noahjohnsmyth@gmail.com>
2021-03-11 11:22:24 +0000

    Create pwgen.js

---
+++ commands/pwgen.js
@@ -0,0 +1,24 @@
+module.exports = {
+  name: "pwgen",
+  category: "NSFW",
+  description: "Sends random hentai",
+  usage: "[command]",
+  execute: async (message, args) => {
+     
+function makeid(length) {
+   var result           = '';
+   var characters       = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"£$%^&*()@{}~:><?';
+   var charactersLength = characters.length;
+   for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+   return result;
+}
+    
+	  if (!args[0]) {
+		  message.author.send('The password I generated was `' + `${makeid(randlength)}` + '`');
+		  
+	  }    
+
+console.log(makeid(randlength));
+  }
