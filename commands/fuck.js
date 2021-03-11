79c659b016fe1a7b4044bbcd5fee970e54d38ff5
Noah Smyth <noahjohnsmyth@gmail.com>
2021-03-11 00:17:59 +0000

    Update fuck.js

--- commands/fuck.js
+++ commands/fuck.js
@@ -7,6 +7,8 @@ module.exports = {
   execute: async (message, args) => {  
 	  if (!message.mentions.users.first()) {
 		  message.channel.send('You need to ping someone if you want to fuck them, silly!');
+		  
+	  }
 	  else {
 	message.channel.send('Okay, whoever got pinged needs to say yes. Thanks!');
     const filter = m => m.content.includes('yes') && m.author.id === message.mentions.users.first().id;
