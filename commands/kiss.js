95b60afac5ee1006a016d849a4f4e1f38ad0ac54
Noah Smyth <noahjohnsmyth@gmail.com>
2021-03-11 23:56:34 +0000

    Update kiss.js

--- commands/kiss.js
+++ commands/kiss.js
@@ -3,22 +3,22 @@ const Discord = require('discord.js')
 const neko = new client();
 
 module.exports = {
-  name: "hug",
+  name: "kiss",
   execute: async (message, args) => {
   //command
 
 
-    let owo = (await neko.sfw.hug());
+    let owo = (await neko.sfw.kiss());
     if (!message.mentions.users.first()) {
         const hug1 = new Discord.RichEmbed()
-        .setTitle(`<@${message.author.id}> hugs themselves`)
+        .setTitle(`<@${message.author.id}> kisses themselves`)
         .setImage(owo.url)
         .setColor(`#FF0000`)
         .setURL(owo.url);
         message.channel.send(hug1);
     } else {
         const hug2 = new Discord.RichEmbed()
-        .setTitle(`<@${message.author.id}> hugs <@${message.mentions.users.first().id}>`)
+        .setTitle(`<@${message.author.id}> kisses <@${message.mentions.users.first().id}>`)
         .setImage(owo.url)
         .setColor(`#FF0000`)
         .setURL(owo.url);
