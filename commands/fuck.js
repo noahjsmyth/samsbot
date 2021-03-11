83fff563ffc2a3281eed3df4d296e298b6b7e673
Noah Smyth <noahjohnsmyth@gmail.com>
2021-03-11 00:16:55 +0000

    Update fuck.js

--- commands/fuck.js
+++ commands/fuck.js
@@ -1,10 +1,13 @@
 const Discord = require('discord.js');
 module.exports = {
-  name: "fucktest",
+  name: "fuck",
   category: "NSFW",
   description: "Sends random hentai",
   usage: "[command]",
   execute: async (message, args) => {  
+	  if (!message.mentions.users.first()) {
+		  message.channel.send('You need to ping someone if you want to fuck them, silly!');
+	  else {
 	message.channel.send('Okay, whoever got pinged needs to say yes. Thanks!');
     const filter = m => m.content.includes('yes') && m.author.id === message.mentions.users.first().id;
 const collector = message.channel.createMessageCollector(filter, { time: 15000000 });
@@ -14,6 +17,7 @@ collector.on('collect', m => {
   message.channel.send('it fucking worked! PogChamp');
 });
                     }
+	  }
  
                     }
                     
