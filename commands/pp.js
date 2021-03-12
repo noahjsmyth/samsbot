047d1af877b13bb1bbf2a777d1b1c90772169d65
Noah Smyth <noahjohnsmyth@gmail.com>
2021-03-12 15:10:02 +0000

    Update pp.js

--- commands/pp.js
+++ commands/pp.js
@@ -21,7 +21,7 @@ function makeid(length) {
 		  message.channel.send(`pp size is 8========================================================D :flushed:`);
 		} else if (message.author.id == gwenid) {
 			message.channel.send('bruh you doesnt even have a pp :fish:');
-		} else if (!message.mentions.members) {
+		} else if (!message.mentions.members.first()) {
 			message.channel.send(`${message.author.tag}'s pp size is 8${makeid(randlength)}D :flushed:`)
 		} else if (message.mentions.users.first().id == ownerid | message.mentions.users.first().id == peterid) {
 			message.channel.send(`${message.mentions.users.fisrt().tag}'s pp size is 8========================================================D :flushed:`);
